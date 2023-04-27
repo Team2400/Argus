665c9736df0292c5e07e5a892a54422b0dd1004a
jonnana <aad336545@gmail.com>
2023-04-27 22:09:29 +0900

    fix: Form1 to DashBoard

--- Argus/Program.cs
+++ Argus/Program.cs
@@ -16,7 +16,7 @@ namespace Argus
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DashBoard());
         }
     }
 }
