d59cfb011c0b8491dcd7cc324d1c8d1c021e3e20
rin00000 <rinjun@naver.com>
2023-05-06 21:17:35 +0900

    dashboard 초기화면

--- Argus/DashBoard.Designer.cs
+++ Argus/DashBoard.Designer.cs
@@ -28,19 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart2 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart3 = new LiveCharts.WinForms.CartesianChart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(26, 27);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(955, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CPU USAGE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.BackColor = System.Drawing.Color.Black;
+            this.cartesianChart1.ForeColor = System.Drawing.Color.Chocolate;
+            this.cartesianChart1.Location = new System.Drawing.Point(23, 55);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(960, 120);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // cartesianChart2
+            // 
+            this.cartesianChart2.BackColor = System.Drawing.Color.Black;
+            this.cartesianChart2.ForeColor = System.Drawing.Color.Chocolate;
+            this.cartesianChart2.Location = new System.Drawing.Point(21, 225);
+            this.cartesianChart2.Name = "cartesianChart2";
+            this.cartesianChart2.Size = new System.Drawing.Size(960, 120);
+            this.cartesianChart2.TabIndex = 2;
+            this.cartesianChart2.Text = "cartesianChart2";
+            // 
+            // cartesianChart3
+            // 
+            this.cartesianChart3.BackColor = System.Drawing.Color.Black;
+            this.cartesianChart3.ForeColor = System.Drawing.Color.Chocolate;
+            this.cartesianChart3.Location = new System.Drawing.Point(21, 406);
+            this.cartesianChart3.Name = "cartesianChart3";
+            this.cartesianChart3.Size = new System.Drawing.Size(960, 120);
+            this.cartesianChart3.TabIndex = 3;
+            this.cartesianChart3.Text = "cartesianChart3";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(23, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(960, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "MEM USAGE";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(21, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(960, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "DISK USAGE";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // DashBoard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1015, 610);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cartesianChart3);
+            this.Controls.Add(this.cartesianChart2);
+            this.Controls.Add(this.cartesianChart1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "DashBoard";
             this.Text = "Argus";
+            this.Load += new System.EventHandler(this.DashBoard_Load);
             this.ResumeLayout(false);
+
         }
 
         #endregion
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart2;
+        private LiveCharts.WinForms.CartesianChart cartesianChart3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Argus/DashBoard.cs
+++ Argus/DashBoard.cs
@@ -7,14 +7,129 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Wpf; //The WPF controls
+using LiveCharts.Defaults;
+using LiveCharts.WinForms;
+using System.Windows.Media;
+using Brushes = System.Windows.Media.Brushes;
 
 namespace Argus
 {
     public partial class DashBoard : Form
     {
+
         public DashBoard()
         {
             InitializeComponent();
+
+            cartesianChart1.Series = new SeriesCollection
+            {
+                 new LineSeries
+                {
+                    Values = new ChartValues<double> {6, 7, 3, 4, 6,5,3,2,6,7,8,7,5},
+                    PointGeometry = null,
+                    LineSmoothness = 0//직선
+                }
+
+            };
+
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                Title="Time",
+                Labels = new[]
+                {
+                "0","5","10","15","20","25","30","35","40","45","50","55","60"//나중에 timespan 문자열로?
+                },
+                Separator = new Separator // force the separator step to 1, so it always display all labels
+                {
+                    Step = 1,
+                    IsEnabled = false //disable it to make it invisible.
+                },
+                LabelsRotation = 0//x축 label 기울기
+            }) ;
+
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                Title="Usage",
+                LabelFormatter = value => value + "%",
+                //LabelFormatter = value => value.ToString("P"),//표준형식지정자 (P) 사용
+                Separator = new Separator()
+            });
+
+            cartesianChart2.Series = new SeriesCollection
+            {
+                 new LineSeries
+                {
+                    Values = new ChartValues<double> {6, 7, 3, 4, 6,5,3,2,6,7,8,7,5},
+                    PointGeometry = null,
+                    LineSmoothness = 0//직선
+                }
+
+            };
+
+            cartesianChart2.AxisX.Add(new Axis
+            {
+                Title = "Time",
+                Labels = new[]
+                {
+                "0","5","10","15","20","25","30","35","40","45","50","55","60"//나중에 timespan 문자열로?
+                },
+                Separator = new Separator // force the separator step to 1, so it always display all labels
+                {
+                    Step = 1,
+                    IsEnabled = false //disable it to make it invisible.
+                },
+                LabelsRotation = 0//x축 label 기울기
+            });
+
+            cartesianChart2.AxisY.Add(new Axis
+            {
+                Title = "Usage",
+                LabelFormatter = value => value + "%",
+                //LabelFormatter = value => value.ToString("P"),//표준형식지정자 (P) 사용
+                Separator = new Separator()
+            });
+
+            cartesianChart3.Series = new SeriesCollection
+            {
+                 new LineSeries
+                {
+                    Values = new ChartValues<double> {6, 7, 3, 4, 6,5,3,2,6,7,8,7,5},
+                    PointGeometry = null,
+                    LineSmoothness = 0//직선
+                }
+
+            };
+
+            cartesianChart3.AxisX.Add(new Axis
+            {
+                Title = "Time",
+                Labels = new[]
+                {
+                "0","5","10","15","20","25","30","35","40","45","50","55","60"//나중에 timespan 문자열로?
+                },
+                Separator = new Separator // force the separator step to 1, so it always display all labels
+                {
+                    Step = 1,
+                    IsEnabled = false //disable it to make it invisible.
+                },
+                LabelsRotation = 0//x축 label 기울기
+            });
+
+            cartesianChart3.AxisY.Add(new Axis
+            {
+                Title = "Usage",
+                LabelFormatter = value => value + "%",
+                //LabelFormatter = value => value.ToString("P"),//표준형식지정자 (P) 사용
+                Separator = new Separator()
+            });
         }
+
+        private void DashBoard_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
