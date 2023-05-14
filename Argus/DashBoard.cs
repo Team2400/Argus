047a3ed7b7def4348aa06e4df6654729b26ab8f8
박현수 <cave100@naver.com>
2023-05-14 15:11:43 +0900

    cpu 리턴 int로 수정, 메모리값 함수 수정, 디스크값 퍼센티지로 리턴하도록 수정

--- Argus/DashBoard.cs
+++ Argus/DashBoard.cs
@@ -21,43 +21,54 @@ namespace Argus
     }
 
     public class Usage
-    {
-        UInt64 Cpu;
-        UInt64 Mem;
-        double Disk = 0;
-
-        public UInt64 getCpuUsage()
+    {        
+        public int getCpuUsage()
         {
+            int Cpu = 0;
             ManagementObjectSearcher cpuusage_info = new ManagementObjectSearcher("select * from Win32_PerfFormattedData_PerfOS_Processor");
             foreach (ManagementObject wmi_CPUUSAGE in cpuusage_info.Get())
             {
-                Cpu = (UInt64)wmi_CPUUSAGE["PercentProcessorTime"];
+                Cpu = int.Parse(wmi_CPUUSAGE["PercentProcessorTime"].ToString());
             }
             return Cpu;
         }
 
-        public UInt64 getMemUsage()
+        public double getMemUsage()
         {
-            ManagementObjectSearcher ram_usage = new ManagementObjectSearcher("select * from Win32_PerfFormattedData_PerfOS_Processor");
-            foreach (ManagementObject wmi_RAMUSAGE in ram_usage.Get())
+            double Mem;
+            double MemSize = 0;
+            double MemUsage = 0;
+            ManagementClass cls = new ManagementClass("Win32_OperatingSystem");
+            ManagementObjectCollection ram = cls.GetInstances();
+            foreach (ManagementObject ram_usage in ram)
             {
-                Mem = (UInt64)wmi_RAMUSAGE["PercentProcessorTime"];
+                MemSize = int.Parse(ram_usage["TotalVisibleMemorySize"].ToString());
+                MemUsage += int.Parse(ram_usage["TotalVisibleMemorySize"].ToString()) - int.Parse(ram_usage["FreePhysicalMemory"].ToString());
             }
+            Mem = (double)MemUsage / MemSize;
+            Mem = Math.Round(Mem, 4) * 100;
             return Mem;
         }
 
         public double getDiskUsage()
         {
+            double DiskUsage = 0;
+            double DiskSize = 0;
+            double DiskPerc;
             DriveInfo[] allDrives = DriveInfo.GetDrives();
             foreach (DriveInfo d in allDrives)
             {
                 if (d.IsReady == true)
                 {
-                    Disk += (double)(d.TotalSize - d.AvailableFreeSpace) / 1024 / 1024 / 1024;
-                    Disk = Math.Round(Disk, 2);
+                    DiskUsage += (double)(d.TotalSize - d.AvailableFreeSpace) / 1024 / 1024 / 1024;
+                    DiskUsage = Math.Round(DiskUsage, 2);
+                    DiskSize += d.TotalSize / 1024 / 1024 / 1024;
+                    DiskSize = Math.Round(DiskSize, 2);
                 }
             }
-            return Disk;
+            DiskPerc = DiskUsage / DiskSize;
+            DiskPerc = Math.Round(DiskPerc, 4) * 100;
+            return DiskPerc;
         }
     }
 }
