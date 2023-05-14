e4c28475c2beaead1031650111e398e1c37fc447
howl201 <skatjdehddms@gmail.com>
2023-05-14 23:14:21 +0900

    Create DB, table and Insert

--- Argus/DashBoard.Designer.cs
+++ Argus/DashBoard.Designer.cs
@@ -28,19 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.sadfasdfasdf = new System.Windows.Forms.Label();
+            this.insert = new System.Windows.Forms.Button();
+            this.print = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // sadfasdfasdf
+            // 
+            this.sadfasdfasdf.AutoSize = true;
+            this.sadfasdfasdf.Location = new System.Drawing.Point(898, 102);
+            this.sadfasdfasdf.Name = "sadfasdfasdf";
+            this.sadfasdfasdf.Size = new System.Drawing.Size(54, 18);
+            this.sadfasdfasdf.TabIndex = 0;
+            this.sadfasdfasdf.Text = "label1";
+            this.sadfasdfasdf.Click += new System.EventHandler(this.test_Click);
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(239, 490);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(154, 69);
+            this.insert.TabIndex = 1;
+            this.insert.Text = "button1";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.insert_Click);
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(789, 476);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(154, 69);
+            this.print.TabIndex = 2;
+            this.print.Text = "button2";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(348, 141);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(397, 227);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // DashBoard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1143, 675);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.print);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.sadfasdfasdf);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "DashBoard";
             this.Text = "Argus";
+            this.Load += new System.EventHandler(this.DashBoard_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label sadfasdfasdf;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Argus/DashBoard.cs
+++ Argus/DashBoard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,68 @@ namespace Argus
 {
     public partial class DashBoard : Form
     {
+        int ID = 0;
+        private LiteDatabase db;
+        private ILiteCollection<SystemUsage> collection;
+        
         public DashBoard()
         {
             InitializeComponent();
+
+        }
+
+        private void DashBoard_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                db = new LiteDatabase(@"ArgusDB.db");
+                collection = db.GetCollection<SystemUsage>("SystemUsage");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void insert_DB(int cUsage, int mUsage, int dUsage)
+        {
+            var data = new SystemUsage { Id = ID++, Timestamp = DateTime.Now, CPU = cUsage, Memory = mUsage, Disk = dUsage };
+            collection.Insert(data);
+        }
+        public class SystemUsage//DB에 들어갈 Data format
+        {
+            public int Id { get; set; }
+            public int CPU { get; set; }
+            public int Memory { get; set; }
+            public int Disk { get; set; }
+            public DateTime Timestamp { get; set; }
+        }
+
+        private void insert_Click(object sender, EventArgs e)
+        {
+            insert_DB(ID, ID, ID);
+        }
+        
+        private ILiteCollection<SystemUsage> selectSysUsage(int limit)
+        {
+            var data = collection.Find(Query.All("Id", Query.Descending), 0, limit);
+            return (ILiteCollection<SystemUsage>)data;
+        }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+            var data = collection.Find(Query.All("Id", Query.Descending), 0, 10);
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
