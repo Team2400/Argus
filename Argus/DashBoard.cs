041f91346faaa3c25e23bd26fa4809f7fc029b96
howl201 <skatjdehddms@gmail.com>
2023-05-27 16:36:43 +0900

    work per unit time

--- Argus/DashBoard.cs
+++ Argus/DashBoard.cs
@@ -21,9 +21,16 @@ namespace Argus
 {
     public partial class DashBoard : Form
     {
-        private SystemUsageDAO systemUsageDAO;
-        public int TIME_INTERVAL = 5 * 1000; //5분단위 시간(밀리초) 5 * 60 * 1000
-        private System.Windows.Forms.Timer timer;
+        private SystemUsageDAO systemUsageDAOMinute;
+        private SystemUsageDAO systemUsageDAOHour;
+        private SystemUsageDAO systemUsageDAODay;
+        public int TIME_INTERVAL_MINUTE = 5 * 1000; //5초
+        public int TIME_INTERVAL_HOUR = 5 * 60 * 1000; //5분
+        public int TIME_INTERVAL_DAY = 2 * 60 * 60* 1000; //2시간
+        private System.Windows.Forms.Timer timerMinute;
+        private System.Windows.Forms.Timer timerHour;
+        private System.Windows.Forms.Timer timerDay;
+        public int checkTimeInterval = 0;//어떤 단위 시간으로 chart를 update할 지 결정하는 변수
 
         public DashBoard()
         {
@@ -38,17 +45,27 @@ namespace Argus
 
         private void DashBoard_Load(object sender, EventArgs e)
         {
-            systemUsageDAO = new SystemUsageDAO("ArgusDB.db", "SystemUsage");
+            systemUsageDAOMinute = new SystemUsageDAO("ArgusDBMinute.db", "SystemUsage");
+            systemUsageDAOHour = new SystemUsageDAO("ArgusDBHour.db", "SystemUsage");
+            systemUsageDAODay = new SystemUsageDAO("ArgusDBDay.db", "SystemUsage");
 
-            timer = new System.Windows.Forms.Timer();
-            timer.Interval = TIME_INTERVAL;//Time Interval
-            timer.Tick += Timer_Tick;// 위 시간에 한번씩 Timer_Tick 호출
-            timer.Start();
+            timerMinute = new System.Windows.Forms.Timer();
+            timerHour = new System.Windows.Forms.Timer();
+            timerDay = new System.Windows.Forms.Timer();
+            timerMinute.Interval = TIME_INTERVAL_MINUTE;//Time Interval
+            timerMinute.Tick += Timer_TickMInute;// 위 시간에 한번씩 Timer_Tick 호출
+            timerMinute.Start();
+            timerHour.Interval = TIME_INTERVAL_HOUR;//Time Interval
+            timerHour.Tick += Timer_TickHour;// 위 시간에 한번씩 Timer_Tick 호출
+            timerHour.Start();
+            timerDay.Interval = TIME_INTERVAL_DAY;//Time Interval
+            timerDay.Tick += Timer_TickDay;// 위 시간에 한번씩 Timer_Tick 호출
+            timerDay.Start();
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void Timer_TickMInute(object sender, EventArgs e)
         {
-            systemUsageDAO.insertDB(
+            systemUsageDAOMinute.insertDB(
                 SystemUsageManager.getCpuUsage(),
                 (int)SystemUsageManager.getMemUsage(),
                 (int)SystemUsageManager.getDiskUsage()
@@ -61,13 +78,15 @@ namespace Argus
             List<DateTime> timeList = new List<DateTime>();
 
             IEnumerable<SystemUsageDTO> data;
-            int count = systemUsageDAO.GetCollection().Count() - 1;//현재까지 저장된 data의 개수이다.
+            int count = systemUsageDAOMinute.GetCollection().Count() - 1;//현재까지 저장된 data의 개수이다.
 
             if (count >= 13)//가져올 data의 상한을 정한다.
                 count = 13;
             
-            data = systemUsageDAO.selectSysUsage(count);//data를 위 count만큼 불러온다
+            data = systemUsageDAOMinute.selectSysUsage(count);//data를 위 count만큼 불러온다
 
+            if (checkTimeInterval != 0)
+                return;
             //data 가공 시작
             foreach (var i in data)//data에 대한 list item 추가
             {   
@@ -111,8 +130,143 @@ namespace Argus
             ArgusChart.updateChart(memoryChart, mList);
             ArgusChart.updateChart(diskChart, dList);
         }
+        private void Timer_TickHour(object sender, EventArgs e)
+        {
+            systemUsageDAOHour.insertDB(
+                SystemUsageManager.getCpuUsage(),
+                (int)SystemUsageManager.getMemUsage(),
+                (int)SystemUsageManager.getDiskUsage()
+            );//data insert at DB
 
-        public List<double> dataModulation(List<double> data, List<DateTime> timeList, int count)
+            List<double> cList = new List<double>();//CPU chart update를 위한 list 이하 동일함
+            List<double> mList = new List<double>();
+            List<double> dList = new List<double>();
+
+            List<DateTime> timeList = new List<DateTime>();
+
+            IEnumerable<SystemUsageDTO> data;
+            int count = systemUsageDAOHour.GetCollection().Count() - 1;//현재까지 저장된 data의 개수이다.
+
+            if (count >= 13)//가져올 data의 상한을 정한다.
+                count = 13;
+
+            data = systemUsageDAOHour.selectSysUsage(count);//data를 위 count만큼 불러온다
+
+            if (checkTimeInterval != 1)
+                return;
+            //data 가공 시작
+            foreach (var i in data)//data에 대한 list item 추가
+            {
+                cList.Add(i.CPU);
+                mList.Add(i.Memory);
+                dList.Add(i.Disk);
+                timeList.Add(i.Timestamp);
+            }
+
+            cList = dataModulation(cList, timeList, count);
+            mList = dataModulation(mList, timeList, count);
+            dList = dataModulation(dList, timeList, count);
+
+            /*TimeSpan destanceOfTime = TimeSpan.Zero;
+            int timeSpace = 0;
+
+            for(int i = 0; i < count - 1; i++)
+            {
+                destanceOfTime = timeList[i] - timeList[i+1];
+                timeSpace = (int)(destanceOfTime.TotalSeconds / 6);//시간 차이를 계산하여 6초 이상이면 그 만큼 0을 삽입
+                if (timeSpace > 0)
+                {
+                    for(int j = 0; j < timeSpace; j++) 
+                    {
+                        dList.Insert(i + 1, 0);
+                        dList.RemoveAt(dList.Count - 1);
+                        cList.Insert(i + 1, 0);
+                        cList.RemoveAt(cList.Count - 1);
+                        mList.Insert(i + 1, 0);
+                        mList.RemoveAt(mList.Count - 1);
+                        timeList.Insert(i + 1, DateTime.Now);
+                        timeList.RemoveAt(timeList.Count - 1);
+                        i++;// 여기서 data추가 량이 많아지면 오류 발생
+                        if (i == count - 1)//따라서 0 삽입의 상한을 지정한다
+                            break;
+                    }
+                }
+            }*/
+
+            ArgusChart.updateChart(cpuChart, cList);//chart update 이하 동일함
+            ArgusChart.updateChart(memoryChart, mList);
+            ArgusChart.updateChart(diskChart, dList);
+        }
+        private void Timer_TickDay(object sender, EventArgs e)
+        {
+            systemUsageDAOMinute.insertDB(
+                SystemUsageManager.getCpuUsage(),
+                (int)SystemUsageManager.getMemUsage(),
+                (int)SystemUsageManager.getDiskUsage()
+            );//data insert at DB
+
+            List<double> cList = new List<double>();//CPU chart update를 위한 list 이하 동일함
+            List<double> mList = new List<double>();
+            List<double> dList = new List<double>();
+
+            List<DateTime> timeList = new List<DateTime>();
+
+            IEnumerable<SystemUsageDTO> data;
+            int count = systemUsageDAOMinute.GetCollection().Count() - 1;//현재까지 저장된 data의 개수이다.
+
+            if (count >= 13)//가져올 data의 상한을 정한다.
+                count = 13;
+
+            data = systemUsageDAOMinute.selectSysUsage(count);//data를 위 count만큼 불러온다
+
+            if (checkTimeInterval != 2)
+                return;
+
+            //data 가공 시작
+            foreach (var i in data)//data에 대한 list item 추가
+            {
+                cList.Add(i.CPU);
+                mList.Add(i.Memory);
+                dList.Add(i.Disk);
+                timeList.Add(i.Timestamp);
+            }
+
+            cList = dataModulation(cList, timeList, count);
+            mList = dataModulation(mList, timeList, count);
+            dList = dataModulation(dList, timeList, count);
+
+            /*TimeSpan destanceOfTime = TimeSpan.Zero;
+            int timeSpace = 0;
+
+            for(int i = 0; i < count - 1; i++)
+            {
+                destanceOfTime = timeList[i] - timeList[i+1];
+                timeSpace = (int)(destanceOfTime.TotalSeconds / 6);//시간 차이를 계산하여 6초 이상이면 그 만큼 0을 삽입
+                if (timeSpace > 0)
+                {
+                    for(int j = 0; j < timeSpace; j++) 
+                    {
+                        dList.Insert(i + 1, 0);
+                        dList.RemoveAt(dList.Count - 1);
+                        cList.Insert(i + 1, 0);
+                        cList.RemoveAt(cList.Count - 1);
+                        mList.Insert(i + 1, 0);
+                        mList.RemoveAt(mList.Count - 1);
+                        timeList.Insert(i + 1, DateTime.Now);
+                        timeList.RemoveAt(timeList.Count - 1);
+                        i++;// 여기서 data추가 량이 많아지면 오류 발생
+                        if (i == count - 1)//따라서 0 삽입의 상한을 지정한다
+                            break;
+                    }
+                }
+            }*/
+
+            ArgusChart.updateChart(cpuChart, cList);//chart update 이하 동일함
+            ArgusChart.updateChart(memoryChart, mList);
+            ArgusChart.updateChart(diskChart, dList);
+        }
+
+        public List<double> dataModulation(List<double> data, List<DateTime> timeList, int count)//data는 가공할 data가 담겨있는 List, timeList는 data의 시간 List, count는 data의 최대 원소 개수
         {
 
             TimeSpan destanceOfTime = TimeSpan.Zero;
@@ -127,7 +281,22 @@ namespace Argus
             for (int i = 0; i < count - 1; i++)
             {
                 destanceOfTime = time[i] - time[i + 1];
-                timeSpace = (int)(destanceOfTime.TotalSeconds / 6);//시간 차이를 계산하여 6초 이상이면 그 만큼 0을 삽입
+                switch(checkTimeInterval)
+                {
+                    case 0:
+                        timeSpace = (int)(destanceOfTime.TotalSeconds / 6);
+                        break;
+                    case 1:
+                        timeSpace = (int)(destanceOfTime.TotalMinutes / 5);
+                        if ((int)(destanceOfTime.TotalMinutes % 5) > 2)//반올림 작업
+                            timeSpace++;
+                        break;
+                    case 2:
+                        timeSpace = (int)(destanceOfTime.TotalHours / 2);
+                        if ((int)(destanceOfTime.TotalHours % 2) > 1)//반올림 작업
+                            timeSpace++;
+                        break;
+                }
                 if (timeSpace > 0)
                 {
                     for (int j = 0; j < timeSpace; j++)
