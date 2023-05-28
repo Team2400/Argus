29cac4618caa2c2ae986f9714f168fddc91d1491
rin00000 <rinjun@naver.com>
2023-05-28 15:19:44 +0900

    feat/CPU alert view

---
+++ Argus/Alert.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Argus
+{
+    partial class Alert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAlertM = new System.Windows.Forms.TextBox();
+            this.textBoxThresH = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 11F);
+            this.label1.Location = new System.Drawing.Point(138, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Threshold";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 11F);
+            this.label2.Location = new System.Drawing.Point(59, 215);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Alert Message";
+            // 
+            // textBoxAlertM
+            // 
+            this.textBoxAlertM.Location = new System.Drawing.Point(385, 215);
+            this.textBoxAlertM.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxAlertM.Multiline = true;
+            this.textBoxAlertM.Name = "textBoxAlertM";
+            this.textBoxAlertM.ReadOnly = true;
+            this.textBoxAlertM.Size = new System.Drawing.Size(509, 206);
+            this.textBoxAlertM.TabIndex = 2;
+            // 
+            // textBoxThresH
+            // 
+            this.textBoxThresH.Location = new System.Drawing.Point(385, 83);
+            this.textBoxThresH.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxThresH.Multiline = true;
+            this.textBoxThresH.Name = "textBoxThresH";
+            this.textBoxThresH.Size = new System.Drawing.Size(509, 54);
+            this.textBoxThresH.TabIndex = 3;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(466, 477);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(196, 54);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(699, 477);
+            this.buttonOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(196, 54);
+            this.buttonOk.TabIndex = 5;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // Alert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(974, 610);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBoxThresH);
+            this.Controls.Add(this.textBoxAlertM);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("굴림", 11F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Alert";
+            this.Text = "Alert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxAlertM;
+        private System.Windows.Forms.TextBox textBoxThresH;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOk;
+    }
+}
