0be355eef926cb19422b611dd0920f438bf744e6
ppiippp <aad336545@gmail.com>
2023-06-10 03:28:49 +0900

    alarm 기능 완성

--- Argus/AlertDialog.Designer.cs
+++ Argus/AlertDialog.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Argus
 {
-    partial class Alert
+    partial class AlertDialog
     {
         /// <summary>
         /// Required designer variable.
@@ -30,88 +30,80 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBoxThresH = new System.Windows.Forms.TextBox();
-            this.buttonCancel = new System.Windows.Forms.Button();
+            this.thresholdTxt = new System.Windows.Forms.TextBox();
             this.buttonOk = new System.Windows.Forms.Button();
-            this.textBoxAlertM = new System.Windows.Forms.TextBox();
+            this.messageTxt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 11F);
-            this.label1.Location = new System.Drawing.Point(138, 83);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(76, 30);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 30);
+            this.label1.Size = new System.Drawing.Size(107, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Threshold";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("굴림", 11F);
-            this.label2.Location = new System.Drawing.Point(59, 215);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(34, 104);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(210, 30);
+            this.label2.Size = new System.Drawing.Size(151, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "Alert Message";
             // 
-            // textBoxThresH
+            // thresholdTxt
             // 
-            this.textBoxThresH.Location = new System.Drawing.Point(359, 83);
-            this.textBoxThresH.Margin = new System.Windows.Forms.Padding(4);
-            this.textBoxThresH.Multiline = true;
-            this.textBoxThresH.Name = "textBoxThresH";
-            this.textBoxThresH.Size = new System.Drawing.Size(510, 55);
-            this.textBoxThresH.TabIndex = 3;
-            // 
-            // buttonCancel
-            // 
-            this.buttonCancel.Location = new System.Drawing.Point(466, 477);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(196, 54);
-            this.buttonCancel.TabIndex = 4;
-            this.buttonCancel.Text = "Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = true;
-            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            this.thresholdTxt.Location = new System.Drawing.Point(200, 30);
+            this.thresholdTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.thresholdTxt.Multiline = true;
+            this.thresholdTxt.Name = "thresholdTxt";
+            this.thresholdTxt.Size = new System.Drawing.Size(268, 31);
+            this.thresholdTxt.TabIndex = 3;
+            this.thresholdTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.thresholdTxt_KeyDown);
+            this.thresholdTxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.thresholdTxt_KeyPress);
             // 
             // buttonOk
             // 
-            this.buttonOk.Location = new System.Drawing.Point(699, 477);
+            this.buttonOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.buttonOk.Location = new System.Drawing.Point(327, 259);
             this.buttonOk.Margin = new System.Windows.Forms.Padding(4);
             this.buttonOk.Name = "buttonOk";
-            this.buttonOk.Size = new System.Drawing.Size(196, 54);
+            this.buttonOk.Size = new System.Drawing.Size(141, 38);
             this.buttonOk.TabIndex = 5;
-            this.buttonOk.Text = "Ok";
+            this.buttonOk.Text = "apply";
             this.buttonOk.UseVisualStyleBackColor = true;
             this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
             // 
-            // textBoxAlertM
+            // messageTxt
             // 
-            this.textBoxAlertM.Location = new System.Drawing.Point(359, 215);
-            this.textBoxAlertM.Multiline = true;
-            this.textBoxAlertM.Name = "textBoxAlertM";
-            this.textBoxAlertM.Size = new System.Drawing.Size(510, 200);
-            this.textBoxAlertM.TabIndex = 6;
+            this.messageTxt.Location = new System.Drawing.Point(200, 104);
+            this.messageTxt.Multiline = true;
+            this.messageTxt.Name = "messageTxt";
+            this.messageTxt.Size = new System.Drawing.Size(268, 109);
+            this.messageTxt.TabIndex = 6;
             // 
-            // Alert
+            // AlertDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(974, 610);
-            this.Controls.Add(this.textBoxAlertM);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(499, 324);
+            this.Controls.Add(this.messageTxt);
             this.Controls.Add(this.buttonOk);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.textBoxThresH);
+            this.Controls.Add(this.thresholdTxt);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("굴림", 11F);
             this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Alert";
+            this.Name = "AlertDialog";
             this.Text = "Alert";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -122,9 +114,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBoxThresH;
-        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox thresholdTxt;
         private System.Windows.Forms.Button buttonOk;
-        private System.Windows.Forms.TextBox textBoxAlertM;
+        private System.Windows.Forms.TextBox messageTxt;
     }
 }
